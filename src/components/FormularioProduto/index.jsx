8695812b1c84809ac268abeb8f68c95dabdbce40
Marcella52 <marcella_hcarvalho@hotmail.com>
2021-11-25 15:13:07 -0300

    Inserido: mais validações, algumas caixas de texto lado a lado e resposividade da página.

--- src/components/FormularioProduto/index.jsx
+++ src/components/FormularioProduto/index.jsx
@@ -1,47 +1,64 @@
 import React from "react";
 import { useFormik } from 'formik';
 
+
 import "./style.css"
 
 /*
 *  VALIDAÇÃO DE CAMPOS DO FORMULARIO DE CADASTRO DE PRODUTO
 *
-* Falta verificar corretamente as quantidades que tem que ser maiores de zero e positivas. Verificar se é numeral que está digitado também.
+* Falta verificar corretamente a data de fabricação e o tamanho minimo e maximo das strings
+* O campo categoria precisa estar relacionada com as categorias
 */
 
 const validate = values => {
   const errors = {};
   if (!values.produto) {
     errors.produto = 'Nome não pode ficar em branco. O campo deve ser preenchido!';
-  } else if (values.produto.length > 4) {
+  } else if (values.produto.length < 4) {
     errors.produto = 'Nome muito curto.';
+  } else if (values.produto.length > 40) {
+    errors.produto = 'Nome muito grande.';
   }
+
 
   if (!values.descricao) {
     errors.descricao = 'Descrição não pode ficar em branco. O campo deve ser preenchido!';
-  } else if (!values.descricao.length > 5) {
+  } else if (values.descricao.length < 5) {
     errors.descricao = 'Sua descrição está muito curta.';
+  } else if (values.descricao.length > 75) {
+    errors.descricao = 'Sua descrição está muito grande.';
   }
+
 
   if (!values.estoque) {
-    errors.estoque = 'A quantidade em estoque não pode ficar em branco.';
+    errors.estoque = 'Valor mínimo permitido é de 1 produto no estoque.';
+  } else if (values.estoque < 0) {
+    errors.estoque = "O estoque não pode ter valor negativo";
   }
 
+  if (values.garantia < 0) {
+    errors.garantia = "A garantia não pode ter valor negativo";
+  }
+
+  var dataAtual = new Date()
   if (!values.fabricacao) {
     errors.fabricacao = 'A data não pode ficar em branco ou ser uma data futura.';
+
+  } else if (values.fabricacao > dataAtual) {
+    errors.fabricacao = "Data inválida, Fabricação não pode estar no futuro!";
+
   }
-
-
-  /*   if (!values.garantia) {
-      errors.garantia = 'A garantia não pode ficar em branco.';
-    } */
-
   if (!values.preco) {
     errors.preco = 'O preço não pode ficar em branco.';
+  } else if (values.preco < 0) {
+    errors.preco = "O preço não pode ter valor negativo";
   }
 
   return errors;
 };
+
+
 
 
 /*
@@ -57,8 +74,8 @@ function FormularioProduto({ produto, setProduto, descricao, setDescricao, estoq
       fabricacao: '',
       garantia: '',
       preco: '',
-      //categoria: ' ',
     },
+
     validate,
     onSubmit: values => {
       alert(JSON.stringify(values, null, 2));
@@ -66,101 +83,109 @@ function FormularioProduto({ produto, setProduto, descricao, setDescricao, estoq
   });
 
 
-
   return (
-    <form className="CadastroProduto" onSubmit={formik.handleSubmit}>
+    <main><h1>Cadastro de produto</h1>
+      <form className="CadastroProduto" onSubmit={formik.handleSubmit}>
+        <label htmlFor="produto">Nome do produto <spam id="alerta">*</spam></label>
+        <input
+          id="produto"
+          nome="produto"
+          type="text"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.produto}
+        />
+        {formik.touched.produto && formik.errors.produto ? <div class="error">{formik.errors.produto}</div> : null}
 
-      <label htmlFor="produto">Nome do produto <spam id="alerta">*</spam></label>
-      <input
-        type="text"
-        id="produto"
-        nome="produto"
-        onChange={formik.handleChange}
-        value={formik.values.produto}
-      />
-      {formik.errors.produto ? <div class="error">{formik.errors.produto}</div> : null}
-
-      <label htmlFor="descricao">Descrição <spam id="alerta">*</spam></label>
-      <input
-        type="text"
-        id="descricao"
-        nome="descricao"
-        onChange={formik.handleChange}
-        value={formik.values.descricao}
-      />
-      {formik.errors.descricao ? <div class="error">{formik.errors.descricao}</div> : null}
-
-
-      <div id="div-meio">
-        <div>
-          <label htmlFor="estoque">Qtd em estoque <spam id="alerta">*</spam></label>
-          <input
-            type="number"
-            id="estoque"
-            nome="estoque"
-            onkeypress="return event.charCode >= 48"
-            min="1"
-            onChange={formik.handleChange}
-            value={formik.values.estoque}
-          />
-          {formik.errors.estoque ? <div class="error">{formik.errors.estoque}</div> : null}
+        <label htmlFor="descricao">Descrição <spam id="alerta">*</spam></label>
+        <input
+          id="descricao"
+          nome="descricao"
+          type="text"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.descricao}
+        />
+        {formik.touched.descricao && formik.errors.descricao ? <div class="error">{formik.errors.descricao}</div> : null}
 
 
+        <div id="div-meio">
+          <div class="esquerda">
+            <label htmlFor="estoque">Qtd em estoque <spam id="alerta">*</spam></label>
+            <input
+              id="estoque"
+              nome="estoque"
+              type="number"
+              onkeypress="return event.charCode >= 48"
+              min="1"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.estoque}
+            />
+            {formik.touched.estoque && formik.errors.estoque ? <div class="error">{formik.errors.estoque}</div> : null}
 
+
+
+          </div>
+          <div class="direita">
+            <label htmlFor="fabricacao">Data de fabricação <spam id="alerta">*</spam></label>
+            <input
+              id="fabricacao"
+              nome="fabricacao"
+              type="date"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.fabricacao}
+            />
+            {formik.touched.fabricacao && formik.errors.fabricacao ? <div class="error">{formik.errors.fabricacao}</div> : null}
+
+          </div>
         </div>
-        <div>
-          <label htmlFor="fabricacao">Data de fabricação <spam id="alerta">*</spam></label>
-          <input
-            type="date"
-            id="fabricacao"
-            nome="fabricacao"
-            onChange={formik.handleChange}
-            value={formik.values.fabricacao}
-          />
-          {formik.errors.fabricacao ? <div class="error">{formik.errors.fabricacao}</div> : null}
-
+        <div id="div-meio">
+          <div class="esquerda">
+            <label htmlFor="garantia">Garantia (meses) <spam id="alerta">*</spam></label>
+            <input
+              id="garantia"
+              nome="garantia"
+              type="number"
+              onkeypress="return event.charCode >= 48"
+              min="0"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.garantia}
+            />
+            {formik.touched.garantia && formik.errors.garantia ? <div class="error">{formik.errors.garantia}</div> : null}
+          </div>
+          <div class="direita">
+            <label htmlFor="preco">Preço unitário <spam id="alerta">*</spam></label>
+            <input
+              id="preco"
+              nome="preco"
+              type="number"
+              placeholder="R$ 00.00"
+              min="0.01"
+              step="0.01"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.preco}
+            />
+            {formik.touched.preco && formik.errors.preco ? <div class="error">{formik.errors.preco}</div> : null}
+          </div>
         </div>
-      </div>
-      <div id="div-meio">
-        <div>
-          <label htmlFor="garantia">Garantia (meses) <spam id="alerta">*</spam></label>
-          <input
-            type="number"
-            id="garantia"
-            nome="garantia"
-            onkeypress="return event.charCode >= 48"
-            min="0"
-            onChange={formik.handleChange}
-            value={formik.values.garantia}
-          />
-          {formik.errors.garantia ? <div class="error">{formik.errors.garantia}</div> : null}
-
-          <label htmlFor="preco">Preço unitário <spam id="alerta">*</spam></label>
-          <input
-            type="number"
-            id="preco"
-            nome="preco"
-            placeholder="R$ 00.00"
-            min="0.01"
-            step="0.01"
-            onChange={formik.handleChange}
-            value={formik.values.preco}
-          />
-          {formik.errors.preco ? <div class="error">{formik.errors.preco}</div> : null}
-        </div>
-      </div>
-      <label>Categoria <spam id="alerta">*</spam></label>
-      <select type="dropdown"
-        nome="categoria">
-        <option value="teste">Opção teste 1</option>
-        <option value="teste">Opção teste 2</option>
-      </select>
+        <label>Categoria <spam id="alerta">*</spam></label>
+        <select
+          nome="categoria"
+          type="dropdown"
+        >
+          <option value="teste">Opção teste 1</option>
+          <option value="teste">Opção teste 2</option>
+        </select>
 
 
-      <input id="botao" type='submit' nome="enviar" value="CADASTRAR PRODUTO" />
+        <input id="botao" type='submit' nome="enviar" value="CADASTRAR PRODUTO" />
 
 
-    </form >
+      </form ></main>
   );
 }
 
