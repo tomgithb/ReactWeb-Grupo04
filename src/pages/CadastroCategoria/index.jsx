1ea9cba5d3606a18d2bab5f221be8cb820917e21
Lucas Vihuchi <lucasvihuchi@gmail.com>
2021-11-28 12:33:44 -0300

    Rodapé adicionado à página de categoria

--- src/pages/CadastroCategoria/index.jsx
+++ src/pages/CadastroCategoria/index.jsx
@@ -1,9 +1,11 @@
 import FormularioCategoria from "../../components/FormularioCategoria";
+import Footer from "../../components/Footer";
 
 function CadastroCategoria() {
     return (
         <>
             <FormularioCategoria />
+            <Footer />
         </>
     );
 }
