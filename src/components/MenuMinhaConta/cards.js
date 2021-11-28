1b876f9d34eb304adda3d0a7a5619ad322033367
Marcella52 <marcella_hcarvalho@hotmail.com>
2021-11-28 00:57:23 -0300

    Adicionadas as imagens de cards e feito os cards do "MinhaConta".

---
+++ src/components/MenuMinhaConta/cards.js
@@ -0,0 +1,74 @@
+import styled from 'styled-components';
+
+
+export const ContainerCards = styled.button`
+    width: 100%;
+    height: 17%;
+    margin: 1.5rem 0rem;
+
+    display: flex;
+    align-items: center;
+  
+    border: none;
+    border-radius: 0.5rem;
+    background-color: var(--cor-caixa-texto);
+   
+    transition-duration: 0.8s;
+    cursor: pointer;
+
+      
+    &:hover {
+        background: var(--cor-fundo-escuro-alternativo);
+     /*    background: radial-gradient(circle, rgba(60,66,80,1) 0%, rgba(1,9,29,1) 100%); */
+        color: var(--cor-texto-claro); 
+    } 
+
+    .texto{
+        width: 100%;
+        margin-left: 2%;
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+        text-align: left;
+    }
+
+
+    .imagem{
+        margin: 0rem 0.5rem;
+        width: 2rem;
+        height: 2rem;
+
+    }
+    .image:hover{
+        -webkit-filter: invert(0%);
+         filter: invert(0%);
+    }
+
+
+    @media screen and (max-width: 790px){
+        width: 20%;
+        height: 4rem;
+        margin-right: 1rem;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-around;
+
+        .texto{
+            display: none;
+        }
+
+        .imagem{
+            width: 40%;
+            height: 40%;
+        }
+   }
+
+
+   @media screen and (max-width: 350px){
+        width: 15%;
+        height: 3rem;
+        margin: 0.5rem;
+   }
+
+`
+
--- src/pages/MinhaConta/index.jsx
+++ src/pages/MinhaConta/index.jsx
@@ -1,9 +1,81 @@
+import React from "react";
+
+import { ContainerCards } from "../../components/MenuMinhaConta/cards";
+import "./style.css"
 
 function MinhaConta() {
     return (
-        <>
-            <h1>Inserir a página minha conta aqui</h1>
-        </>
+        <main className="container-minha-conta">
+            <div>
+                <h1>Inserir a página minha conta aqui</h1>
+            </div>
+
+            <div className="corpo">
+
+                <div className="lateral">
+
+                    <ContainerCards onClick={() => alert('Ops! Você clicou.')}>
+
+                        <img src="./img/user.png" className="imagem" alt="Ícone de usuário" />
+
+                        <div className="texto">
+                            <h3>Cadastro</h3>
+                            <p>Alterar as informações pessoais e de endereço</p>
+                        </div>
+                    </ContainerCards>
+
+
+
+                    <ContainerCards onClick={() => alert('Ops! Você clicou.')}>
+
+                        <img src="./img/compras.png" className="imagem" alt="Ícone de um carrinho" />
+
+                        <div className="texto">
+                            <h3>Compras</h3>
+                            <p>Visualizar pedidos realizados</p>
+                        </div>
+                    </ContainerCards>
+
+
+                    <ContainerCards onClick={() => alert('Ops! Você clicou.')}>
+
+                        <img src="./img/vendas.png" className="imagem" alt="Símbolo de uma mão com uma seta subindo na diagonal" />
+
+                        <div className="texto">
+                            <h3>Vendas</h3>
+                            <p>Visualizar vendas realizadas</p>
+                        </div>
+                    </ContainerCards>
+
+
+                    <ContainerCards onClick={() => alert('Ops! Você clicou.')}>
+
+                        <img src="./img/cadastrarProduto.png" className="imagem" alt="Símbolo de uma mão segurando uma caixa aberta" />
+
+                        <div className="texto">
+                            <h3>Cadastrar produtos</h3>
+                            <p>Cadastrar novos produtos para venda</p>
+                        </div>
+                    </ContainerCards>
+
+
+                    <ContainerCards onClick={() => alert('Ops! Você clicou.')}>
+
+                        <img src="./img/cadastrarCategoria.png" className="imagem" alt="Símbolo de uma lista" />
+
+                        <div className="texto">
+                            <h3>Cadastrar categorias</h3>
+                            <p>Cadastrar novas categorias</p>
+                        </div>
+                    </ContainerCards>
+
+                </div>
+
+
+                <div><h2>Cantinho do Wellington</h2></div>
+
+            </div>
+        </main >
     );
 }
 
